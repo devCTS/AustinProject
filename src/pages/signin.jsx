68c7e583f50a0350924f3667acc857f8bd03da47
tvorexspecialist <tvorexspecialist@gmail.com>
2022-10-17 05:25:35 -0700

    Migrated to HTTPS

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -23,7 +23,7 @@ export default function Layout() {
 
     const data = state;
     try {
-        const res = await axios.post("http://3.144.202.225:8080/api/v1/auth/login", {email: state.email, password:state.password});
+        const res = await axios.post("https://3.144.202.225:8080/api/v1/auth/login", {email: state.email, password:state.password});
         if(res.data.status){
           setUser(res.data);
           navigate("/staging");

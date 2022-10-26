407be58fa275020237465f155b7b3529255661e3
devCTS <dev@catalyst.sh>
2022-10-26 19:35:41 +0530

    added discord authentication

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,95 +1,179 @@
-import React, { useState } from "react"
-import Header1 from "../components/header-1";
-import Footer1 from "../components/footer-1";
+import React, { useState, useEffect } from "react";
+import { withPrefix, Link, navigate } from "gatsby";
+import { useLocation } from "@reach/router";
 import axios from "axios";
 import Helmet from "react-helmet";
-import { withPrefix, Link, navigate } from "gatsby";
-import { setUser, handleFacebookSignIn, handleGoogleSignIn } from "../services/auth";
 import $ from "jquery";
 
+import Header1 from "../components/header-1";
+import Footer1 from "../components/footer-1";
 
+import {
+  setUser,
+  handleDiscordSignIn,
+  handleGoogleSignIn,
+  isBrowser,
+} from "../services/auth";
 
 export default function Layout() {
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+  const query = useQuery();
 
   const [state, setState] = useState({
     email: "",
     password: "",
   });
-  
+
+  useEffect(() => {
+    if (query.get("redirect") === "discord") {
+      if (query.get("code")) {
+        handleDiscordSignIn(query.get("code"));
+      }
+    }
+  }, []);
 
   const onChange = (e) => {
-    setState({ ...state, [e.target.name]: e.target.value })
-  }
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const data = state;
     try {
-        const res = await axios.post("https://back-end-99gens.herokuapp.com/api/v1/auth/login", {email: state.email, password:state.password}); 
-        if(res.data.status){
-          setUser(res.data);
-          navigate("/staging");
-        }
+      const res = await axios.post(
+        "https://back-end-99gens.herokuapp.com/api/v1/auth/login",
+        { email: state.email, password: state.password }
+      );
+      if (res.data.status) {
+        setUser(res.data);
+        navigate("/staging");
+      }
     } catch (error) {
-      $('#password').parents('li').find('.required-txt').remove();
-        $('#password').after(`
+      $("#password").parents("li").find(".required-txt").remove();
+      $("#password").after(`
             <div class="required-txt">* Invalid Email or Password</div>
         `);
     }
   };
 
-    return (
-      <>
+  return (
+    <>
       <div className="container-main" id="page">
-      <Helmet>
-        <link href={withPrefix("assets/css/progress-bar.css")} rel="stylesheet"/>
-        <script src={withPrefix("assets/js/progress-bar.js")} type="text/javascript" />
-      </Helmet>
+        <Helmet>
+          <link
+            href={withPrefix("assets/css/progress-bar.css")}
+            rel="stylesheet"
+          />
+          <script
+            src={withPrefix("assets/js/progress-bar.js")}
+            type="text/javascript"
+          />
+        </Helmet>
         <Header1></Header1>
         <main className="content-main">
           <div className="form-container">
-              <div className="container">
+            <div className="container">
               <div className="container-box">
-                  <div className="sign-in-form">
+                <div className="sign-in-form">
                   <div className="box">
-                      <div className="heading-top">
+                    <div className="heading-top">
                       <h3>Sign in</h3>
-                      <p>or <Link to ="/create-account">Create an Account</Link></p>
-                      </div>
-                      <div className="form-field">
+                      <p>
+                        or <Link to="/create-account">Create an Account</Link>
+                      </p>
+                    </div>
+                    <div className="form-field">
                       <form action="#" method="post">
-                          <div className="input-out">
-                            <label>Email</label>
-                            <input id="email" name="email" type="text" value={state.email} onChange={onChange} placeholder="name@domain.com"/>
-                          </div>
-                          <div className="input-out">
-                            <label>Password</label>
-                            <li className="signinli">
-                              <input id="password" name="password" type="password" value={state.password} onChange={onChange} />
-                            </li>
-                          </div>
-                          <div className="btn-out"><button className="btn" onClick={handleSubmit}>Sign in</button></div>
+                        <div className="input-out">
+                          <label>Email</label>
+                          <input
+                            id="email"
+                            name="email"
+                            type="text"
+                            value={state.email}
+                            onChange={onChange}
+                            placeholder="name@domain.com"
+                          />
+                        </div>
+                        <div className="input-out">
+                          <label>Password</label>
+                          <li className="signinli">
+                            <input
+                              id="password"
+                              name="password"
+                              type="password"
+                              value={state.password}
+                              onChange={onChange}
+                            />
+                          </li>
+                        </div>
+                        <div className="btn-out">
+                          <button className="btn" onClick={handleSubmit}>
+                            Sign in
+                          </button>
+                        </div>
                       </form>
-
-                      </div>
-                      <div className="or-circle"><span>OR</span></div>
-                      <div className="bottom-btns">
+                    </div>
+                    <div className="or-circle">
+                      <span>OR</span>
+                    </div>
+                    <div className="bottom-btns">
                       <ul>
-                          <li><a ><em><img src={withPrefix("assets/img/icon-apple.png")} alt="icon-apple" /></em> Apple</a></li>
-                          {/* <button onClick={handleGoogleSignIn} >This is it!!!!</button> */}
-                          <li><a onClick={handleGoogleSignIn} ><em><img src={withPrefix("assets/img/icon-google.png")} alt="icon-google" /></em> Google</a></li>
-                          <li><a onClick={handleFacebookSignIn}><em><img src={withPrefix("assets/img/icon-discord.png")} alt="icon-discord" /></em> Discord</a></li>
+                        <li>
+                          <a>
+                            <em>
+                              <img
+                                src={withPrefix("assets/img/icon-apple.png")}
+                                alt="icon-apple"
+                              />
+                            </em>{" "}
+                            Apple
+                          </a>
+                        </li>
+                        {/* <button onClick={handleGoogleSignIn} >This is it!!!!</button> */}
+                        <li>
+                          <a onClick={handleGoogleSignIn}>
+                            <em>
+                              <img
+                                src={withPrefix("assets/img/icon-google.png")}
+                                alt="icon-google"
+                              />
+                            </em>{" "}
+                            Google
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            onClick={() => {
+                              isBrowser() &&
+                                window.open(
+                                  process.env.DISCORD_CODE_REDIRECT_URI,
+                                  "_parent"
+                                );
+                            }}
+                          >
+                            <em>
+                              <img
+                                src={withPrefix("assets/img/icon-discord.png")}
+                                alt="icon-discord"
+                              />
+                            </em>{" "}
+                            Discord
+                          </a>
+                        </li>
                       </ul>
-                      </div>
+                    </div>
                   </div>
-                  </div>
+                </div>
               </div>
-              </div>
+            </div>
           </div>
         </main>
         <Footer1></Footer1>
       </div>
-      </>
-    )
+    </>
+  );
 }
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,20 +1,24 @@
 import { navigate } from "gatsby";
 import "isomorphic-fetch";
+import axios from "axios";
+import Cookies from "universal-cookie";
 import firebase from "firebase/app";
 import "firebase/auth";
-import firebaseConfig from '../services/firebase.config';
+import firebaseConfig from "../services/firebase.config";
 
+const cookies = new Cookies();
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 // const facebookProvider = new firebase.auth.FacebookAuthProvider();
 
-export const setUser = user =>
+export const setUser = (user) =>
   window.localStorage.setItem("gatsbyUser", JSON.stringify(user));
 
 export const handleGoogleSignIn = () => {
-  firebase.auth()
+  firebase
+    .auth()
     .signInWithPopup(googleProvider)
     .then((result) => {
       const credential = result.credential;
@@ -22,17 +26,58 @@ export const handleGoogleSignIn = () => {
       const user = result.user;
       setUser(user);
       navigate("/staging");
-
-    }).catch((error) => {
+    })
+    .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       const email = error.email;
       const credential = error.credential;
     });
-}
+};
 
+export const handleDiscordSignIn = async (code) => {
+  try {
+    // getting access token from discord
+    const data = new URLSearchParams({
+      client_id: process.env.DISCORD_CLIENT_ID,
+      client_secret: process.env.DISCORD_CLIENT_SECRET,
+      grant_type: "authorization_code",
+      code: code.toString(),
+      redirect_uri: process.env.DISCORD_REDIRECT_URI,
+    });
+    const response = await axios.post(
+      `${process.env.DISCORD_API_URL}/oauth2/token`,
+      data.toString(),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    cookies.set("discord_access_token", response?.data?.access_token, {
+      path: "/",
+      maxAge: response?.data?.expires_in,
+    });
+    localStorage.setItem(
+      "discord_refresh_token",
+      JSON.stringify(response?.data?.refresh_token)
+    );
 
-
+    // getting user data from discord
+    const userDetails = await axios.get(
+      `${process.env.DISCORD_API_URL}/users/@me`,
+      {
+        headers: {
+          Authorization: `Bearer ${cookies.get("discord_access_token")}`,
+        },
+      }
+    );
+    setUser(userDetails?.data);
+    navigate("/staging");
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
 
 //const SERVER = ``;
 
@@ -47,20 +92,20 @@ export const getUser = () =>
 
 // Save user data to localstorage
 
-
 // Return true if user is logged in. Otherwise false.
 export const isLoggedIn = () => {
   const user = getUser();
-  return (!!user.status | !!user.email);
+  return !!user.status | !!user.email;
 };
 
-export const logout = callback => {
+export const logout = (callback) => {
   setUser({});
-  navigate('/signin');
+  navigate("/signin");
+  cookies.remove("discord_access_token", { path: "/" });
   // callback();
 };
 
-export const validateEmail = email => {
-    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-    return emailReg.test( email );
-}
+export const validateEmail = (email) => {
+  var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  return emailReg.test(email);
+};
